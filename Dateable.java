1193ae8d832017b4855b6da6bee65041ed33c27e
emilyluo33 <124020677+emilyluo33@users.noreply.github.com>
2023-02-08 09:31:42 +0800

    Create Dateable.java

---
+++ Dateable.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Dateable {
+
+    public static void main(String[] args) {
+        Scanner age = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int yourage = age.nextInt();
+        int minage = (yourage/2) + 7;
+        System.out.println(yourage + " year olds should data somebody who is at least " + minage + " years old.");
